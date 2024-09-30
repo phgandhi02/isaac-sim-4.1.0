bc0ae4e6e2c9c3ccbf8c630152cf2f9fc6d80858
Andrew Chen <achen26@ncsu.edu>
2024-09-30 12:29:41 -0400

    created exampleScene which is supposed to create multiple robot stations.

---
+++ extsUser/exampleScene/exampleScene_python/scenario.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2022-2023, NVIDIA CORPORATION. All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto. Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+
+
+class ScenarioTemplate:
+    def __init__(self):
+        pass
+
+    def setup_scenario(self):
+        pass
+
+    def teardown_scenario(self):
+        pass
+
+    def update_scenario(self):
+        pass
+
+
+import numpy as np
+from omni.isaac.core.utils.types import ArticulationAction
+
+"""
+This scenario takes in a robot Articulation and makes it move through its joint DOFs.
+Additionally, it adds a cuboid prim to the stage that moves in a circle around the robot.
+
+The particular framework under which this scenario operates should not be taken as a direct
+recomendation to the user about how to structure their code.  In the simple example put together
+in this template, this particular structure served to improve code readability and separate
+the logic that runs the example from the UI design.
+"""
+
+
+class ExampleScenario(ScenarioTemplate):
+    def __init__(self):
+        pass
+
+    def setup_scenario(self, robot, teeths):
+        self._robot = robot
+        self._teeth = teeths
+        pass
+
+    def teardown_scenario(self):
+        pass
+    def update_scenario(self, step: float):
+        # self._robot.apply_action(ArticulationAction(joint_positions=np.array([0,-71.5,64.4,-83.0,251.9,-179.3])))
+        pass
